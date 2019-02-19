ed56275e084e1c3287f4fc096ca13992b3d8817a
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-02-19 20:51:04 +0100

    Add caching

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var cache = {};
+
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
+    console.debug(request);
+    cache[request.from] = (request.st === 9);
+    console.debug(cache);
 });
