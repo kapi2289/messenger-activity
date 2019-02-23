ded263cf4f08aeb14bba0e7e7572f27dc75660e0
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-02-23 19:28:57 +0100

    Add options page

---
+++ src/options/options.js
@@ -0,0 +1,16 @@
+function saveOptions(e) {
+    browser.storage.sync.set({
+        showNotifications: document.querySelector("#show-notifications").checked
+    });
+    e.preventDefault();
+}
+
+function restoreOptions() {
+    let gettingItem = browser.storage.sync.get('showNotifications');
+    gettingItem.then(res => {
+        document.querySelector("#show-notifications").checked = res.showNotifications || false;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
