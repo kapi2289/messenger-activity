184adcf660fbeb16dc1daa9d6a50878ca11584c4
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-03-26 16:06:45 +0100

    Fix options

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,9 +1,8 @@
 import * as $ from 'jquery'
 import defaults from '../defaults'
-import ChangeEvent = JQuery.ChangeEvent;
 import Event = JQuery.Event;
 
-async function saveOptions(e: ChangeEvent) {
+async function saveOptions(e: Event) {
     await browser.storage.sync.set({
         // showNotifications: $("#show-notifications")[0].checked,
         borderColor: $("#border-color").val()
@@ -11,12 +10,12 @@ async function saveOptions(e: ChangeEvent) {
     e.preventDefault();
 }
 
-function restoreOptions(e: Event) {
+function restoreOptions() {
     browser.storage.sync.get(['showNotifications', 'borderColor']).then((res) => {
         // $("#show-notifications")[0].checked = res.showNotifications || defaults.showNotifications;
         $("#border-color").val(res.borderColor as string || defaults.borderColor);
     });
 }
 
-$(document).on("ready", restoreOptions);
+$(restoreOptions);
 $('input').on("change", saveOptions);
